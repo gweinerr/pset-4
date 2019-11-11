a1cb9c92e0890ecaaa858b0c9a4f32b3396bf236
Gabrielle Weiner <gweiner@ucvts.org>
2019-11-11 13:11:11 -0500

    fixed spacing for exercise one

--- src/sum.js
+++ src/sum.js
@@ -6,4 +6,4 @@ const MAX = Number.MAX_SAFE_INTEGER;
 const lowerBound = Number(readlineSync.question("\nLower bound: "));
 const upperBound = Number(readlineSync.question("Upper bound: "));
 const lowerBound2 = Number(readlineSync.question("Lower bound: "));
-const upperBound2 = Number(readlineSync.question("Upper bound: \n"));
+const upperBound2 = Number(readlineSync.question("Upper bound: "));
