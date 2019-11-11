3724c49ca0635e254890dd5f9ccfca980ba95ae8
Gabrielle Weiner <gweiner@ucvts.org>
2019-11-11 13:02:16 -0500

    started exercise one

---
+++ src/sum.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+const lowerBound = Number(readlineSync.question("\nLower bound: "));
+const upperBound = Number(readlineSync.question("Upper bound: "));
+const lowerBound2 = Number(readlineSync.question("Lower bound: "));
+const upperBound2 = Number(readlineSync.question("Upper bound: \n"));
