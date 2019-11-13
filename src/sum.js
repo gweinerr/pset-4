865dc877d059e7b8cc818231ba86c5c5dfdf16c0
Gabrielle Weiner <gweiner@ucvts.org>
2019-11-13 13:17:39 -0500

    finished exercise one

--- src/sum.js
+++ src/sum.js
@@ -3,5 +3,27 @@ const readlineSync = require("readline-sync");
 const MIN = Number.MIN_SAFE_INTEGER;
 const MAX = Number.MAX_SAFE_INTEGER;
 
-const lowerBound = Number(readlineSync.question("\nLower bound: "));
-const upperBound = Number(readlineSync.question("Upper bound: "));
+let lowerBound = 1;
+let upperBound = 0;
+
+console.log();
+
+while (lowerBound > upperBound || upperBound > MAX || upperBound < MIN || lowerBound < MIN || lowerBound > MAX ||
+       Number.isNaN(upperBound) || !Number.isInteger(upperBound) || Number.isNaN(lowerBound) || !Number.isInteger(lowerBound)) {
+  lowerBound = Number(readlineSync.question("Lower bound: "));
+  upperBound = Number(readlineSync.question("Upper bound: "));
+}
+
+let sum = 0;
+let i = 0;
+
+console.log();
+
+if (upperBound % 2 === 0) {
+for (let i = lowerBound; i <= upperBound; i = i + 2)
+  sum = sum + i
+}
+
+sum = sum.toLocaleString(2);
+
+console.log(sum + ".");
