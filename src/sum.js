411ec721e61f902601dc489a385b14f32a50d262
Gabrielle Weiner <gweiner@ucvts.org>
2019-11-13 12:09:27 -0500

    merging repos

--- src/sum.js
+++ src/sum.js
@@ -5,5 +5,3 @@ const MAX = Number.MAX_SAFE_INTEGER;
 
 const lowerBound = Number(readlineSync.question("\nLower bound: "));
 const upperBound = Number(readlineSync.question("Upper bound: "));
-const lowerBound2 = Number(readlineSync.question("Lower bound: "));
-const upperBound2 = Number(readlineSync.question("Upper bound: "));
