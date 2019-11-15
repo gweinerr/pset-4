bf0e5e7c14662e892822272ba565eccf00012671
Gabrielle Weiner <gweiner@ucvts.org>
2019-11-15 13:13:33 -0500

    started exercise 2

--- src/reverse.js
+++ src/reverse.js
@@ -2,3 +2,16 @@ const readlineSync = require("readline-sync");
 
 const MIN = 1;
 const MAX = Number.MAX_SAFE_INTEGER;
+
+positiveInteger = Number(readlineSync.question("\nPositive integer: "));
+
+while (Number.isNaN(positiveInteger) || !Number.isInteger(positiveInteger) || positiveInteger > MAX || positiveInteger < MIN) {
+  positiveInteger = Number(readlineSync.question("Positive integer: "));
+}
+
+while (positiveInteger > 0) {
+  let digit = positiveInteger % 10
+  positiveInteger = Math.floor(positiveInteger / 10);
+
+console.log(digit)
+}
