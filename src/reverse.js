62c658d93410fb4fbe5002c16a7a41bc6478038d
EMILY STEINER <emilysteiner@EMILYs-iMac.local>
2019-11-17 15:56:34 -0500

    corrected exercise two.

--- src/reverse.js
+++ src/reverse.js
@@ -4,6 +4,10 @@ const MIN = 1;
 const MAX = Number.MAX_SAFE_INTEGER;
 
 positiveInteger = Number(readlineSync.question("\nPositive integer: "));
+let digit;
+let order = "";
+
+
 
 while (Number.isNaN(positiveInteger) || !Number.isInteger(positiveInteger) || positiveInteger > MAX || positiveInteger < MIN) {
   positiveInteger = Number(readlineSync.question("Positive integer: "));
@@ -12,6 +16,12 @@ while (Number.isNaN(positiveInteger) || !Number.isInteger(positiveInteger) || po
 while (positiveInteger > 0) {
   let digit = positiveInteger % 10
   positiveInteger = Math.floor(positiveInteger / 10);
+  if (positiveInteger >= 1) {
+    order = order + digit + ", "
+  }
+  else [
+    order = order + digit + ".\n"
+  ]
+ }
 
-console.log(digit)
-}
+ console.log("\n" + order);
