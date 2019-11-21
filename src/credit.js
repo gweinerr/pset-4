7264d77c0a977a403fad35db00bbb161bb68950b
Gabrielle Weiner <gweiner@ucvts.org>
2019-11-21 13:19:52 -0500

    started exercise ten

--- src/credit.js
+++ src/credit.js
@@ -2,8 +2,18 @@ const readlineSync = require("readline-sync");
 
 let MIN = 1000000000000;
 let MAX = 9999999999999999;
+let sum = 0;
 
 console.log();
 do {
   creditNum = Number(readlineSync.question("Number: "));
 } while (creditNum < MIN || creditNum > MAX || Number.isNaN(creditNum) || !Number.isInteger(creditNum))
+
+
+// step one
+//check to see if it is invalid or not (LONG PART)
+
+creditNum = String(creditNum);
+let length = creditNum.length;
+
+if (length == 15 && (charAtIndex(0, 1) == (3, 4) || charAtIndex(0, 1) == (3, 7)))
